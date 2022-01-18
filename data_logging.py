bf085bd4c7af31483c0fdd542f24dd8c5a1e4b54
ccxex29 <blackccxex@gmail.com>
2022-01-18 22:56:45 +0700

    Add logging on server

---
+++ data_logging.py
@@ -0,0 +1,91 @@
+"""
+Data Logging Module
+"""
+
+from typing import Union, Optional
+from os.path import join
+from os import makedirs
+import logging
+from uuid import uuid4
+
+import sqlite3
+from cv2 import cv2
+import numpy as np
+
+class DataLogging():
+    """
+    Data Logging Class
+    """
+    def __init__(self, path: Optional[str] = None, table_name: Optional[str] = None):
+        self.database = None
+        self.cursor = None
+        self.data_id = None
+        self.database_name = 'log.db'
+        self.table_name = table_name or 'logs'
+        self.path = path or './logs'
+        self.image_path = 'images'
+        self._init_log_directory()
+        self._init_database()
+        self.set_new_id()
+
+    def _init_database(self):
+        self.database = self._get_database()
+        if self.database is not None:
+            self.cursor = self.database.cursor()
+            self.cursor.execute(
+                f'''
+                CREATE TABLE IF NOT EXISTS {self.table_name} (
+                    id TEXT,
+                    architecture_name TEXT,
+                    detection_result TEXT,
+                    confidence DOUBLE,
+                    pos_x INTEGER,
+                    pos_y INTEGER,
+                    pos_w INTEGER,
+                    pos_h INTEGER
+                )
+                ''')
+
+    def _init_log_directory(self):
+        try:
+            makedirs(join(self.path, self.image_path))
+        except OSError:
+            return
+
+    def set_new_id(self):
+        """
+        Change current id to a new UUID
+        """
+        self.data_id = uuid4().hex
+
+    def _get_database(self) -> Optional[sqlite3.Connection] :
+        try:
+            database = sqlite3.connect(join(self.path, self.database_name))
+        except sqlite3.Error:
+            logging.error('Database could not be initialised.')
+            return None
+        return database
+
+    def log_image(self, image: np.ndarray):
+        """
+        Log Image to File
+        """
+        cv2.imwrite(join(self.path, self.image_path, self.data_id + '.jpg'), image, [cv2.IMWRITE_JPEG_QUALITY, 70])
+
+    def log_metrics(self, data: dict[str, Union[str, int, float, None]]):
+        """
+        Log Textual Metric Data
+        """
+        if self.cursor is None:
+            return
+        self.cursor.execute(f'INSERT INTO {self.table_name} (id, detection_result, confidence, architecture_name, pos_x, pos_y, pos_w, pos_h) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [
+            self.data_id,
+            data['detection_result'],
+            data['confidence'],
+            data['architecture_name'],
+            data['pos_x'],
+            data['pos_y'],
+            data['pos_w'],
+            data['pos_h']
+        ])
+        self.database.commit()
--- main.py
+++ main.py
@@ -15,7 +15,9 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.websocket
 import tornado.options
+import numpy as np
 
+from data_logging import DataLogging
 from model_memo import ModelMemo
 from data_processor import DataProcessor
 from model_selection import ModelSelection
@@ -89,6 +91,51 @@ class WsChannelHandler(tornado.websocket.WebSocketHandler): # pylint: disable=W0
             return
         self.model_selection = model_selection # pylint: disable=W0201
 
+    def _set_data_logging(self, data_logging: Optional[DataLogging]):
+        if not isinstance(data_logging, DataLogging):
+            return
+        self.data_logging = data_logging # pylint: disable=W0201
+
+    def _log_result_to_file(self, image: np.ndarray, data: dict) -> None:
+        if self.data_logging is None:
+            return
+        self.data_logging.set_new_id()
+        self.data_logging.log_image(image)
+        self.data_logging.log_metrics(data)
+
+    def _get_result_string(self, prediction): # pylint: disable=no-self-use
+        """
+        Get Enumeration String
+        """
+        if not isinstance(prediction, Prediction):
+            return 'n/a'
+        return 'drowsy' if prediction == Prediction.DROWSY else 'alert'
+
+    def _log_result(self, data_processor: DataProcessor, prediction_a, prediction_b) -> None:
+        if not data_processor.logging:
+            return
+
+        data = {
+            'pos_x': data_processor.face_detection['position']['x'],
+            'pos_y': data_processor.face_detection['position']['y'],
+            'pos_w': data_processor.face_detection['position']['w'],
+            'pos_h': data_processor.face_detection['position']['h'],
+            'raw': data_processor.face_detection
+        }
+        data.update({
+            'detection_result': self._get_result_string(prediction_a['prediction']),
+            'confidence': prediction_a['confidence'],
+            'architecture_name': data_processor.architecture_a
+        })
+        self._log_result_to_file(data_processor.image, data)
+        if prediction_a is not prediction_b:
+            data.update({
+                'detection_result': self._get_result_string(prediction_b['prediction']),
+                'confidence': prediction_b['confidence'],
+                'architecture_name': data_processor.architecture_b
+            })
+            self._log_result_to_file(data_processor.image, data)
+
     def _load_architecture(self, name: str):
         """
         Load Architecture to Model
@@ -106,13 +153,14 @@ class WsChannelHandler(tornado.websocket.WebSocketHandler): # pylint: disable=W0
             prediction_b = self._load_architecture(data_processor.architecture_b).trigger_predict(data_processor.face_detection['face_image'])
         return prediction_a, prediction_b
 
-    def initialize(self, memo = None, model_selection = None):
+    def initialize(self, memo = None, model_selection = None, data_logging = None):
         """
         Tornado WS Init
         """
         self._set_channel(None)
         self._set_memo(memo)
         self._set_model_selection(model_selection)
+        self._set_data_logging(data_logging)
 
     def set_default_headers(self):
         self.set_header('Access-Control-Allow-Origin', '*')
@@ -140,18 +188,20 @@ class WsChannelHandler(tornado.websocket.WebSocketHandler): # pylint: disable=W0
                 return
             prediction_a, prediction_b = self._get_prediction_objects(data_processor)
 
+            self._log_result(data_processor, prediction_a, prediction_b)
+
             self.write_message(json.dumps({
                 'data': {
                     'architecture_a': {
                         'name': data_processor.architecture_a,
                         'canonical_name': self.model_selection.get_model_canonical_name(data_processor.architecture_a),
-                        'detection': 'drowsy' if prediction_a['prediction'] == Prediction.DROWSY else 'alert',
+                        'detection': self._get_result_string(prediction_a['prediction']),
                         'confidence': prediction_a['confidence']
                     },
                     'architecture_b': {
                         'name': data_processor.architecture_b,
                         'canonical_name': self.model_selection.get_model_canonical_name(data_processor.architecture_b),
-                        'detection': 'drowsy' if prediction_b['prediction'] == Prediction.DROWSY else 'alert',
+                        'detection': self._get_result_string(prediction_b['prediction']),
                         'confidence': prediction_b['confidence']
                     },
                     'position': data_processor.get_face_detection()['position'],
@@ -182,7 +232,7 @@ class WsChannelHandler(tornado.websocket.WebSocketHandler): # pylint: disable=W0
     def check_origin(self, origin):
         return True
 
-def make_app(memo: ModelMemo, model_selection: ModelSelection):
+def make_app(memo: ModelMemo, model_selection: ModelSelection, data_logging: DataLogging):
     """
     Create Tornado App
     """
@@ -192,7 +242,8 @@ def make_app(memo: ModelMemo, model_selection: ModelSelection):
         }),
         WsChannelHandler.url({
             'memo': memo,
-            'model_selection': model_selection
+            'model_selection': model_selection,
+            'data_logging': data_logging
         })
     ])
 
@@ -208,7 +259,8 @@ def load_memo(model_selection: ModelSelection):
 if __name__ == "__main__":
     logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
     g_model_selection = ModelSelection()
-    app = make_app(load_memo(g_model_selection), g_model_selection)
+    g_data_logging = DataLogging()
+    app = make_app(load_memo(g_model_selection), g_model_selection, g_data_logging)
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(PORT)
     logging.info('Listening on port %s', PORT)
